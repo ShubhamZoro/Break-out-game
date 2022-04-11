340769245bb58c4a0c748947510e501db868fbf2
ShubhamZoro <89676909+ShubhamZoro@users.noreply.github.com>
2022-04-11 22:22:36 +0530

    just adding the pause the function in the game.

--- main.py
+++ main.py
@@ -15,47 +15,57 @@ score=Scoreboard()
 ball=Ball()
 paddle=Paddle((-300,-260))
 screen.listen()
-screen.onkeypress(paddle.right_mov,"Right")
-screen.onkeypress(paddle.left_mov,"Left")
+
 
 bricks=Brick()
 bricks.makeRow()
 screen.update()
 is_on=True
-
+is_paused=False
+def stopMovement():
+    global is_paused
+    if is_paused == True:
+        is_paused = False
+    else:
+        is_paused = True
+screen.onkeypress(paddle.right_mov, "Right")
+screen.onkeypress(paddle.left_mov, "Left")
+screen.onkeypress(stopMovement, 'space')
 
 while is_on:
-    time.sleep(ball.move_speed)
-    screen.update()
-    ball.move()
-    for brick in bricks.row:
-        if ball.distance(brick)<33:
-            brick.penup()
-            brick.goto(1000,1000)
+    if not is_paused:
+        time.sleep(ball.move_speed)
+        screen.update()
+        ball.move()
+        for brick in bricks.row:
+            if ball.distance(brick)<33:
+                brick.penup()
+                brick.goto(1000,1000)
+                ball.bounce_y()
+                score.scores()
+
+        if ball.xcor()>380 or ball.xcor()<-380:
+            ball.bounce_x()
+
+        #to detect collision with the r_paddle
+        if ball.distance(paddle)<33 and ball.ycor()<-240:
             ball.bounce_y()
-            score.scores()
 
-    if ball.xcor()>380 or ball.xcor()<-380:
-        ball.bounce_x()
-
-    #to detect collision with the r_paddle
-    if ball.distance(paddle)<33 and ball.ycor()<-240:
-        ball.bounce_y()
-
-    if ball.ycor()>288:
-        ball.bounce_y()
+        if ball.ycor()>288:
+            ball.bounce_y()
 
 
-    if ball.ycor()<-288:
-        is_on = False
+        if ball.ycor()<-288:
+            is_on = False
 
-        score.result("You lose")
+            score.result("You lose")
 
 
-    if score.score==10:
-        is_on = False
-        score.result("You win")
-
-        # Turtle.write("You Win", align="center",font= ('Courier', 30, 'italic'))
+        if score.score==10:
+            is_on = False
+            score.result("You win")
+    else:
+        screen.update()
+            # Turtle.write("You Win", align="center",font= ('Courier', 30, 'italic'))
 
 screen.mainloop()
